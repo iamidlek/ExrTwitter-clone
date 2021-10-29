f95206e56cfcac95e05c8e97787cc55a6e4b9e07
iamidlek <iamidlek@naver.com>
2021-10-29 16:28:10 +0900

    login setting

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,2 +1,57 @@
-const Auth = () => <span>auth</span>
-export default Auth
+import { useState } from "react";
+import { authService } from "fbase";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { type, value },
+    } = event;
+    if (type === "email") {
+      setEmail(value);
+    } else if (type === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if(newAccount) {
+        data = await authService.createUserWithEmailAndPassword(email,password)
+      } else {
+        data = await authService.signInWithEmailAndPassword(email,password)
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "create Account" : "Login"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+export default Auth;
