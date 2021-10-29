d2cd6a2555c6dbb80744f26d5da934a7a8b09512
iamidlek <iamidlek@naver.com>
2021-10-29 16:47:51 +0900

    email login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { type, value },
@@ -25,9 +26,11 @@ const Auth = () => {
         data = await authService.signInWithEmailAndPassword(email,password)
       }
     } catch(error) {
-      console.log(error)
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -46,10 +49,14 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? "create Account" : "Login"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google">Continue with Google</button>
+        <button name="gh">Continue with Github</button>
       </div>
     </div>
   );
