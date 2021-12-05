d2f4d9a3858e98307b029467e5ffc35006fcaf6f
iamidlek <iamidlek@naver.com>
2021-12-06 00:09:56 +0900

    realtime get data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,25 +5,21 @@ import {
   addDoc,
   serverTimestamp,
   orderBy,
-  getDocs,
+  onSnapshot,
   query,
 } from "firebase/firestore";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      // nweets컬렉션 추가
-      const docRef = await addDoc(collection(dbService, "nweets"), {
-        nweet,
-        createdAt: serverTimestamp(),
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
+    // nweets컬렉션 추가
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: serverTimestamp(),
+      creatorId: userObj.uid,
+    });
 
     setNweet("");
   };
@@ -33,22 +29,18 @@ const Home = () => {
     } = event;
     setNweet(value);
   };
-  const getNweets = async () => {
-    // 여러 doc 가져오기
-    const q = query(collection(dbService, "nweets"));
-    // 스냅샷에는 많은 정보가 들어있다
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() => nweet, createdAt;
-      const nweetObj = {
-        ...doc.data(),
-        id: doc.id,
-      };
-      setNweets((prev) => [nweetObj, ...prev]);
-    });
-  };
+
   useEffect(() => {
-    getNweets();
+    onSnapshot(
+      query(collection(dbService, "nweets"), orderBy("createdAt", "desc")),
+      (snapshot) => {
+        const nweetArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNweets(nweetArray);
+      }
+    );
   }, []);
   return (
     <div>
@@ -65,7 +57,7 @@ const Home = () => {
       <div>
         {nweets.map((nweet) => (
           <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
+            <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
