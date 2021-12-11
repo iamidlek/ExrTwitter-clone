aeb8dd20d9c2226fa63b6e3b5ba314675f5ed291
iamidlek <iamidlek@naver.com>
2021-12-12 00:17:03 +0900

    feat: style authfrom

---
+++ src/components/AuthForm.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  getAuth,
+} from "firebase/auth";
+
+const AuthForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { type, value },
+    } = event;
+    if (type === "email") {
+      setEmail(value);
+    } else if (type === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(getAuth(), email, password);
+      } else {
+        data = await signInWithEmailAndPassword(getAuth(), email, password);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  return (
+    <>
+      <form onSubmit={onSubmit} className="container">
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+          className="authInput"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+          className="authInput"
+        />
+        <input
+          type="submit"
+          className="authInput authSubmit"
+          value={newAccount ? "create Account" : "Login"}
+        />
+        {error && <span className="authError">{error}</span>}
+      </form>
+      <span onClick={toggleAccount} className="authSwitch">
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+    </>
+  );
+};
+
+export default AuthForm;
