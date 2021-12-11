bf280691935e043f69bc5a7690ee1d333f95ba5f
iamidlek <iamidlek@naver.com>
2021-12-12 00:15:09 +0900

    feat: add style and firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { firebaseInstance } from "fbase";
+import "./styles.css";
+
 ReactDOM.render(
   // <React.StrictMode>
   <App />,
